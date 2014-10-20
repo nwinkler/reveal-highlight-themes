b0952104e07f948581283738108b58e008fdd12e
Nils Winkler <nils@nilswinkler.com>
2014-10-20 12:02:39 +0200

    Added configuration for converting the Highlight.js themes into the Reveal.js format.
    Added all of the Highlight.js 8.3.0 themes in the Reveal.js format.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,56 @@
 module.exports = function (grunt) {
-    'use strict';
+  'use strict';
 
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-    
-    // Project configuration
-    grunt.initConfig({
-        // Metadata
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-            ' Licensed <%= props.license %> */\n',
-    });
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    // Default task
-    grunt.registerTask('default', []);
+  // Project configuration
+  grunt.initConfig({
+
+    replace: {
+      hljs: {
+        src: ['bower_components/highlightjs/styles/*.css'],
+        dest: 'styles/',
+        replacements: [{
+          from: '.hljs-',
+          to: '.'
+        }, {
+          from: '.hljs',
+          to: 'pre code'
+        },{
+          from: /^\./mg,
+          to: 'pre .'
+        }]
+      }
+    },
+
+    bump: {
+      options: {
+        files: [
+          'package.json',
+          'bower.json',
+          'README.md'
+        ],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION% #noissue',
+        commitFiles: ['-a'], // '-a' for all files
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin master',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+      }
+    }
+
+  });
+
+  // Default task
+  grunt.registerTask('build', [
+    'replace:hljs'
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
 };
